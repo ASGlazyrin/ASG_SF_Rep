c0b896dc57eac7e993119559cfa905fe7a38890b
ASGlazyrin <alexander_glazyrin@mail.ru>
2022-11-13 16:54:40 +0300

    Final task

--- project_0/README.md
+++ project_0/README.md
@@ -3,10 +3,6 @@
 ## Оглавление
 [1. Описание проекта](https://github.com/ASGlazyrin/ASG_SF_Rep/tree/main/project_0/README.md#Описание-проекта)
 [2. Какой кейс решаем](https://github.com/ASGlazyrin/ASG_SF_Rep/tree/main/project_0/README.md#Какой-кейс-решаем)
-[3. Краткая информация о данных](https://github.com/ASGlazyrin/ASG_SF_Rep/tree/main/project_0/README.md#Краткая-информация-о-данных)
-[4. Этапы работы над проектом](https://github.com/ASGlazyrin/ASG_SF_Rep/tree/main/project_0/README.md#Этапы-работы-над-проектом)
-[5. Результаты](https://github.com/ASGlazyrin/ASG_SF_Rep/tree/main/project_0/README.md#Результаты)
-[6. Выводы](https://github.com/ASGlazyrin/ASG_SF_Rep/tree/main/project_0/README.md#Выводы)
 
 ### Описание проекта
 Угадать загаданное компьютерное число за минимальное число попыток.
@@ -25,8 +21,5 @@
 
 **Что практикуем**
 Учимся писать хороший код на Python
-
-### Краткая информация о данных
-....
 
 :arrow_up:[к оглавлению](https://github.com/ASGlazyrin/ASG_SF_Rep/tree/main/project_0/README.md#Оглавление)
--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def random_predict(number:int=1) -> int:
+
+def random_predict(number:int=np.random.randint(1, 101)) -> int:
     """Рандомно угадываем число
 
     Args:
@@ -10,28 +11,41 @@ def random_predict(number:int=1) -> int:
         int: Число попыток
     """
 
-    count = 0
+    count = 0 # Cчетчик попыток
+    max_number = 100 # Максимальное значение рассматриваемого интервала
+    min_number = 1 # Минимальное значение рассматриваемого интервала
+    predict_number = np.random.randint(1, 101) # предполагаемое число    
 
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101) # предполагаемое число
-        if number == predict_number:
-            break # выход из цикла, если угадали
-    return(count)
+        
+        if predict_number > number:
+             max_number = predict_number - 1
+             predict_number = (max_number + min_number) // 2        
+        elif predict_number < number:
+             min_number = predict_number + 1
+             predict_number = (max_number + min_number) // 2        
+        else:
+            break # выход из цикла, если угадали    
+    
+    return count
+
 
 def score_game(random_predict) -> int:
-    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
+    """
+    За какое количество попыток в среднем из 1000 подходов угадывает алгоритм
 
     Args:
         random_predict ([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
+        
     """
 
     count_ls = [] # список для сохранения количества попыток
     np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+    random_array = np.random.randint(1, 101, size=(1000)) # список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
@@ -39,7 +53,9 @@ def score_game(random_predict) -> int:
     score = int(np.mean(count_ls)) # находим среднее количество попыток
 
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    
     return(score)
+
 
 # RUN
 if __name__ == '__main__':
